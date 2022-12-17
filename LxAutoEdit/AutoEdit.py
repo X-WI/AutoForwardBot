cb2be992df3b9f39401f154e510a99341f36f24c
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 03:14:09 +0530

    Update AutoEdit.py

--- LxAutoEdit/AutoEdit.py
+++ LxAutoEdit/AutoEdit.py
@@ -1,28 +1,20 @@
-import pyrogram 
+from info import Info
 import logging
 from pyrogram import Client, filters, enums
-
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-
-media_filter = filters.document | filters.video | filters.audio
-usercaption_position = "top"
-caption_position = usercaption_position.lower()
-caption_text = "@HQFilms4U"
-
-
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(bot, message):
-        await message.reply("<I>I Am Auto Caption Bot Just Add me as a Admin in your channel with edit permission and See Magic</I>\n\n• **support** : @Hollywood_0980\n• **Source** : https://github.com/0AIB/TG-Caption-Bot")
+        await message.reply("Hello there, I'm Auto Caption Bot! Join @Lx_0980")
 
 
 @Client.on_message(pyrogram.filters.channel)
 async def editing(bot, message):
       try:
          media = message.document or message.video or message.audio
-         caption_text = "@HQFilms4U"
+         caption_text = Info.CAPTION_TXT
       except:
          caption_text = ""
          pass 
