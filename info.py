ff4d7ee6e169eb8db9a5398ca1717c88e6dcd1f3
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 03:24:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,5 +4,5 @@ class Info(object):
       API_HASH = "7cce2717e7e89eb534b7da973926f6c4"
       API_ID = 26686963
       BOT_TOKEN = "5300015911:AAE0jwmVovvrv6zH9dC8TfE5-UlFIVOyX78"
-      
+      CAPTION_TXT = "@Lx_0980"
       CHANNEL_ID = 1743048821
