4ecca626d0e2e04655a3d116c4f0710b112f7fd1
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 02:06:45 +0530

    Create Copy.py

---
+++ LxAutoEdit/Copy.py
@@ -0,0 +1,16 @@
+import os, time
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters
+from info import Info
+
+@Client.on_message(filters.media)
+async def forward(bot, update):
+    try:
+        await bot.copy_message(
+            chat_id=Info.CHANNEL_ID,
+            from_chat_id=update.chat.id,
+            message_id=update.message_id,
+            caption=update.caption
+        )
+    except FloodWait as e:
+        time.sleep(e.x)
