f56552d9ea4e17c5190e1458ae454747d1bc5655
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 02:33:06 +0530

    Update Copy.py

--- LxAutoEdit/Copy.py
+++ LxAutoEdit/Copy.py
@@ -12,10 +12,10 @@ logger = logging.getLogger(__name__)
 async def forward(bot, update):
     try:
         await bot.copy_message(
-            chat_id=Info.CHANNEL_ID,
-            from_chat_id=update.chat.id,
-            message_id=update.id,
-            caption=update.caption
+            chat_id = Union[Info.CHANNEL_ID],
+            from_chat_id = Union[update.chat.id],
+            message_id = update.message.id,
+            caption = update.caption
         )
     except FloodWait as e:
         time.sleep(e.value)
