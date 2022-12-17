72e9ad3879c8cc67b689ab2244d589656365f4b1
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 00:44:05 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,7 @@
+from os import getenv
+
+class Info(object):
+      API_HASH = "7cce2717e7e89eb534b7da973926f6c4"
+      API_ID = 26686963
+      BOT_TOKEN = "5300015911:AAE0jwmVovvrv6zH9dC8TfE5-UlFIVOyX78"
+      
