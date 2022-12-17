a4e8a992be597e0653ed9d46631d8aa605ca79fd
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 02:56:49 +0530

    Update Copy.py

--- LxAutoEdit/Copy.py
+++ LxAutoEdit/Copy.py
@@ -10,7 +10,7 @@ async def forward(bot, update):
     try:
         await bot.copy_message(
             chat_id =Info.CHANNEL_ID,
-            from_chat_id=update.id,
+            from_chat_id=update.chat.id,
             message_id=update.id,
             caption=update.caption
         )
