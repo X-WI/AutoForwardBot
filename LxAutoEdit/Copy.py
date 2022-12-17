2bce44c4ffcad231399bb5db49586b92e45290ce
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 02:18:08 +0530

    Update Copy.py

--- LxAutoEdit/Copy.py
+++ LxAutoEdit/Copy.py
@@ -1,7 +1,12 @@
 import os, time
 from pyrogram.errors import FloodWait
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums 
 from info import Info
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 
 @Client.on_message(filters.media)
 async def forward(bot, update):
@@ -9,7 +14,7 @@ async def forward(bot, update):
         await bot.copy_message(
             chat_id=Info.CHANNEL_ID,
             from_chat_id=update.chat.id,
-            message_id=update.message.id,
+            message_id=update.id,
             caption=update.caption
         )
     except FloodWait as e:
