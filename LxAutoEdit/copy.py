dd639ee116aa1514c049e2ad56d6c231bc86b95e
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 10:05:43 +0530

    Update copy.py

--- LxAutoEdit/copy.py
+++ LxAutoEdit/copy.py
@@ -3,10 +3,12 @@ import os, asyncio
 from pyrogram.errors import FloodWait
 from pyrogram import Client, filters, enums
 
-
+CHANNEL_ID = 1001743048821
+replace_text = "@Lx0980"
+"""
 @Client.on_message(filters.media)
 async def forward(bot, update):
-    try:
+    try:      
         await bot.copy_message(
             chat_id=-1001743048821,
             from_chat_id=update.chat.id,
@@ -14,5 +16,31 @@ async def forward(bot, update):
             caption=update.caption,
             parse_mode=enums.ParseMode.MARKDOWN
         )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+"""
+
+@Client.on_message(filters.media)
+async def forward(bot, update):
+      try:
+         media = message.document or message.video or message.audio
+         replace_text = "@Lx0980"
+      except:
+         replace_text = ""
+         pass 
+      if (message.document or message.video or message.audio): 
+          if message.caption:                        
+             file_caption = f"**{message.caption}**"                
+
+      try:
+          if caption_position == "hello":
+             await bot.copy_message(
+                 chat_id=CHANNEL_ID, 
+                 from_chat_id=update.chat.id,
+                 message_id=update.id,
+                 caption=file_caption + "\n" + replace_text,
+                 parse_mode=enums.ParseMode.MARKDOWN
+             )
+
     except FloodWait as e:
         await asyncio.sleep(e.value)
