3e44f9689d0eda535306404aaebfaaffb739ced6
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 09:47:30 +0530

    Create copy.py

---
+++ LxAutoEdit/copy.py
@@ -0,0 +1,18 @@
+import os, asyncio
+
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters, enums
+
+
+@Client.on_message(filters.media)
+async def forward(bot, update):
+    try:
+        await bot.copy_message(
+            chat_id=-1001743048821,
+            from_chat_id=update.chat.id,
+            message_id=update.id,
+            caption=update.caption
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
