ebe0e21851eacf6280b3ba3c973bcc3eb082711a
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 10:33:47 +0530

    Update copy.py

--- LxAutoEdit/copy.py
+++ LxAutoEdit/copy.py
@@ -1,10 +1,6 @@
-import os, asyncio,logging
+import os, asyncio
 from pyrogram.errors import FloodWait
 from pyrogram import Client, filters, enums
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 
 CHANNEL_ID = 1001743048821
 replace_text = "@Lx0980"
@@ -25,7 +21,8 @@ async def forward(bot, update):
 
 @Client.on_message(filters.media)
 async def forward(bot, update):                     
-      try:                       await bot.copy_message(
+      try: 
+             await bot.copy_message(
                  chat_id=CHANNEL_ID, 
                  from_chat_id=update.chat.id,
                  message_id=update.id,
