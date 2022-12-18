e2998e89eb6bee7b90e33a15c9a4fe6178d79d23
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 10:38:43 +0530

    Update copy.py

--- LxAutoEdit/copy.py
+++ LxAutoEdit/copy.py
@@ -3,8 +3,7 @@ from pyrogram.errors import FloodWait
 from pyrogram import Client, filters, enums
 
 CHANNEL_ID = 1001743048821
-replace_text = "@Lx0980"
-"""
+
 @Client.on_message(filters.media)
 async def forward(bot, update):
     try:      
@@ -12,23 +11,8 @@ async def forward(bot, update):
             chat_id=-1001743048821,
             from_chat_id=update.chat.id,
             message_id=update.id,
-            caption=update.caption,
+            caption=update.caption + "@Lx0980",
             parse_mode=enums.ParseMode.MARKDOWN
         )
     except FloodWait as e:
         await asyncio.sleep(e.value)
-"""
-
-@Client.on_message(filters.media)
-async def forward(bot, update):                     
-      try: 
-             await bot.copy_message(
-                 chat_id=CHANNEL_ID, 
-                 from_chat_id=update.chat.id,
-                 message_id=update.id,
-                 caption=update.caption  + "\n" + replace_text,                
-                 parse_mode=enums.ParseMode.MARKDOWN,
-             )
-
-      except FloodWait as e:
-          await asyncio.sleep(e.value)
