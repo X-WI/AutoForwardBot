ff1f57767aca98011638b98b7cfe7c69d2551b7c
WENTRAX <127533917+wentrax@users.noreply.github.com>
2023-04-01 18:00:18 +0530

    Update start.py

--- LxAutoEdit/start.py
+++ LxAutoEdit/start.py
@@ -4,15 +4,12 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
     buttons = [[
-        InlineKeyboardButton('📜 Channel', url='https://t.me/Lx0980_Official'),
+        InlineKeyboardButton('📜 Channel', url='https://t.me/DFF_UPDATES'),
         InlineKeyboardButton('Owner ♻️', url='https://t.me/Lx_0980')
-    ],[
-        InlineKeyboardButton('SouceCode 💡', url='https://github.com/Lx0988')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
         chat_id=message.chat.id,
         reply_markup=reply_markup,
-        text=Translation.START_TXT.format(
-                message.from_user.first_name),
+        text=  "Hello there, my name is Auto Caption Bot! I can edit Channel media Caption. <b>Developer: @Lx_0980<\b>"              
         parse_mode=enums.ParseMode.HTML)
