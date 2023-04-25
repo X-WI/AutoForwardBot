5c06959190d19449d2281ab31896d27f0009fb55
WENTRAX <127533917+wentrax@users.noreply.github.com>
2023-04-26 01:04:00 +0530

    Update and rename AutoEdit.py to DFF_to_DX.py

---
+++ LxAutoEdit/DFF_to_DX.py
@@ -0,0 +1,35 @@
+import os, asyncio, logging
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters, enums
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+media_filter = filters.document | filters.video 
+
+@Client.on_message(filters.chat(-1001667023505) & media_filter)
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001563784107, 
+            from_chat_id=-1001667023505, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**",          
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+
+@Client.on_message(filters.chat(-1001667023505) & media_filter)
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001563784107, 
+            from_chat_id=-1001667023505, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**",          
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+
+N
