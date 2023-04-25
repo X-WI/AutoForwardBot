9b9193477cf43124f228ce03d818aae964f1756a
WENTRAX <127533917+wentrax@users.noreply.github.com>
2023-04-26 02:10:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ from os import getenv
 class Info(object):
       API_HASH = "128061b2dad0483ed28816ac9f5ee346"
       API_ID = 7300958
-      BOT_TOKEN = "6258379531:AAG3NMSKkqpFcRgtpWkk5-cLq_of5qqN9j4"
+      BOT_TOKEN = "6042212691:AAEr3g_jsWndNc7rMQp3Of_Zk4N-thVPYkU"
       CAPTION_TXT = "@Lx_0980"
       CHANNEL_ID = 1743048821
