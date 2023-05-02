28d28d2abbab4123f10480908b1d74e959dde70c
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-02 19:17:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ from os import getenv
 class Info(object):
       API_HASH = "128061b2dad0483ed28816ac9f5ee346"
       API_ID = 7300958
-      BOT_TOKEN = "6042212691:AAEr3g_jsWndNc7rMQp3Of_Zk4N-thVPYkU"
+      BOT_TOKEN = "6039073773:AAHxCn5HTwM-0GU6FdWzJTLYB9-w2Z59SIc"
       CAPTION_TXT = "@Lx_0980"
       CHANNEL_ID = 1743048821
