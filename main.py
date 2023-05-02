b31f7ae4dc5bcf90d2d9243794a2241e05998248
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-02 19:13:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,48 +2,39 @@ import logging
 import pyrogram
 from pyrogram import Client, filters, enums
 from info import Info
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(lineno)d - %(module)s - %(levelname)s - %(message)s'
+)
+logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-"""
-class lxautoedit(Client):
-    
-            def __init__(self):
+import uvloop
+uvloop.install()
+from config import Config
+from pyrogram import Client 
+
+
+class Bot(Client, Info):
+    def __init__(self):
         super().__init__(
-            name = "Auto-edit",
-            bot_token = Info.BOT_TOKEN,
-            api_id = Info.API_ID,
-            api_hash = Info.API_HASH,           
-            workers = 20,
-            plugins = dict(
-                root="LxAutoEdit"
-            )
+            name="CHANNELFORWARD",
+            bot_token=self.BOT_TOKEN,
+            api_id=self.API_ID,
+            api_hash=self.API_HASH,
+            workers=20,
+            plugins={'root': 'Plugins'}
         )
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        print(f"New session started for {me.first_name}({me.username})")
+
+    async def stop(self):
+        await super().stop()
+        print("Session stopped. Bye!!")
 
 
 if __name__ == "__main__" :
-    lxautoedit().run()
-"""
-
-
-class Bot(Client):
-
-    def __init__(self):
-        super().__init__(
-            name="autobot",
-            api_hash=Info.API_HASH,
-            api_id=Info.API_ID,
-            bot_token=Info.BOT_TOKEN,            
-            plugins={
-                "root": "LxAutoEdit"
-            },
-        )
-
-print("Bot started!")
-app = Bot()
-app.run()
+    Bot().run()
