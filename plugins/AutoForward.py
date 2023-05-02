b01f909841add36b18e7e6820135ca24ef21cf22
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-02 23:04:06 +0530

    Update AutoForward.py

--- plugins/AutoForward.py
+++ plugins/AutoForward.py
@@ -10,7 +10,7 @@ media_filter = filters.document | filters.video
 async def forward(bot, update):
     try:      
         await bot.copy_message(
-            chat_id=-1001842801591, 
+            chat_id=-1001956270124, 
             from_chat_id=-1001667023505, 
             message_id=update.id, 
             caption=f"**{update.caption}**",          
@@ -23,7 +23,7 @@ async def forward(bot, update):
 async def forward2(bot, update):
     try:      
         await bot.copy_message(
-            chat_id=-1001842801591, 
+            chat_id=-1001956270124, 
             from_chat_id=-1001277498778, 
             message_id=update.id, 
             caption=f"**{update.caption}**",          
