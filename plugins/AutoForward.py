de3d2f062b1d139352e238ac8ebbcacdddb1b3c8
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-05 17:41:28 +0530

    Update AutoForward.py

--- plugins/AutoForward.py
+++ plugins/AutoForward.py
@@ -4,6 +4,8 @@ import logging
 logger = logging.getLogger(__name__)
 from main import Bot
 
+
+dff_caption = "➠ @Hollywood_0980\n➠ @DFF_UPDATES"
 media_filter = filters.document | filters.video 
 
 @Bot.on_message(filters.chat(-1001667023505) & media_filter)
@@ -27,6 +29,21 @@ async def forward2(bot, update):
             from_chat_id=-1001277498778, 
             message_id=update.id, 
             caption=f"**{update.caption}**",          
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except Exception as e:
+        logger.exception(e)
+        
+        
+       
+@Bot.on_message(filters.chat(-1001976541395) & media_filter)
+async def forward2(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001937462586, 
+            from_chat_id=-1001976541395, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**" + "\n\n" + f"**{dff_caption}**",       
             parse_mode=enums.ParseMode.MARKDOWN                     
         )
     except Exception as e:
